31cdecb3872cbaf321afffccd6a70f57ab08640b
Luiz de Freitase <luizdefreitas10@gmail.com>
2022-09-24 11:25:18 -0300

    att

--- my-port/my-portfolio/src/pages/Projects.js
+++ my-port/my-portfolio/src/pages/Projects.js
@@ -10,17 +10,21 @@ function Projects () {
       <Header />
       <div className="father-div-projects">
         <a href="https://react-trivia-eta.vercel.app/" target="_blank" rel="noreferrer">
-          <img src="TriviaApp.png" alt="Trivia Login Page Img"
-          height="150px"
-          width="150px"
+          <div className="img-class-projects">
+            <img src="TriviaApp.png" alt="Trivia Login Page Img"
+            height="150px"
+            width="150px"
           />
+          </div>
           <h1>Trivia Game App</h1>
         </a>
         <a href="https://trybe-wallet-lac.vercel.app/" target="_blank" rel="noreferrer">
-          <img src="trybewallet.png" alt="TrybeWallet Login Page Img"
-          height="150px"
-          width="150px"
-          />
+          <div className="img-class-projects">
+            <img src="trybewallet.png" alt="TrybeWallet Login Page Img"
+            height="150px"
+            width="150px"
+            />
+          </div>
           <h1>TrybeWallet App</h1>
         </a>
       </div>
